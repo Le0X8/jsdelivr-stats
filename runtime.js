668c6a560907acf5bb3a38a36b2b9eae0c6e94f3
LeoX DEV <84378319+Le0X8@users.noreply.github.com>
2023-06-09 13:47:48 +0200

    Added some strings

--- runtime.js
+++ runtime.js
@@ -13,19 +13,37 @@ google.charts.setOnLoadCallback(async () => {
     const dataArr = [];
     Object.keys(network.hits.providers[0].dates).forEach(date => dataArr.push([(new Date(date)).toLocaleDateString()]));
     network.hits.providers.forEach((provider, i) => Object.keys(provider.dates).forEach((date, j) => {
-        dataArr[j][i + 1] = provider.dates[date].total;
+        dataArr[j][i + 1] = provider.dates[date].total / 1000000000;
     }));
 
     data.addRows(dataArr);
 
     var options = {
         chart: {
-            title: 'jsDelivr Hits per day in last year',
+            title: 'jsDelivr Hits per day in ' + (new Date(Object.keys(network.hits.providers[0].dates))[0]),
             subtitle: 'in billions'
         }
     };
 
+    main.appendChild((name => {
+        var el = document.createElement('span');
+        el.classList.add('info');
+        el.innerText = name;
+        return el;
+    })('jsDelivr has served a total of ' + network.hits.total.toLocaleString() + ' requests in ' + (new Date(Object.keys(network.hits.providers[0].dates))[0])));
 
+    main.appendChild((name => {
+        var el = document.createElement('span');
+        el.classList.add('title');
+        el.innerText = name;
+        return el;
+    })('jsDelivr Hits per day in ' + (new Date(Object.keys(network.hits.providers[0].dates))[0])));
+    main.appendChild((name => {
+        var el = document.createElement('span');
+        el.classList.add('subtitle');
+        el.innerText = name;
+        return el;
+    })('in billions'));
     var chart = new google.visualization.LineChart(main.appendChild(document.createElement('div')));
 
     chart.draw(data, google.charts.Line.convertOptions(options));
